def671957774f0a400f7075a8e45119631055c43
kazuma kumagai <s19C1041JE@s.chibakoudai.jp>
2021-01-14 23:15:05 +0000

    変更

---
+++ catkin_ws/build/tsnpkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/rsys2/catkin_ws/src/tsnpkg
+# Build directory: /home/ubuntu/rsys2/catkin_ws/build/tsnpkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
