f2ae1bfec7c5fbbeec216b6ede42d1f5c4a78141
kazumakumagai <71998849+kazumakumagai@users.noreply.github.com>
2021-01-25 09:16:19 +0900

    Delete build directory

--- build/tsnpkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/rsys2/src/tsnpkg
-# Build directory: /home/ubuntu/rsys2/build/tsnpkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
